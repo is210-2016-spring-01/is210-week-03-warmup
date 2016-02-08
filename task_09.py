b8660e4eb8f55ad7b6765389699ceef3fb8fe301
pcwebb <pamela.webb@spsmail.cuny.edu>
2016-02-08 06:03:11 +0000

    Uploading Wk 3 warm-up

--- task_09.py
+++ task_09.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""A computer that can build the question."""
+"""A computer that can build the question. And then it can answer the
+questions of the universe.
+"""
 
 THE_ANSWER_TO_EVERYTHING = 42
 
