600efa2335741bbfefc97338b39cd2771f5808cb
Elisha Chauhan <elisha.chauhan@spsmail.cuny.edu>
2016-02-12 06:25:12 +0000

    Modified file for TURTLE_POWER

--- task_05.py
+++ task_05.py
@@ -5,3 +5,10 @@
 
 TEENAGE_MUTANT_NINJAS = ('Michaelangelo. Leonardo. Rafael. Donatello. Heroes '
                          'in a half shell.')
+
+print TEENAGE_MUTANT_NINJAS.split('. ')
+
+TURTLE_POWER = (['Michaelangelo', 'Leonardo', 'Rafael', 'Donatello', 'Heroes '
+                'in a half shell.'])
+
+print TURTLE_POWER
