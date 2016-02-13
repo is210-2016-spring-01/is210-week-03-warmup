69c6eaef48d6918bb651685dd7fd9d65f72c7f63
jaiz25 <jaizelle@gmail.com>
2016-02-13 11:17:23 +0000

    Task 9

--- task_09.py
+++ task_09.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """A computer that can build the question."""
+"""Adding a multi-line docstring to task_09.py.  This docstring should break
+
+across two paragraphs.
+"""
+
 
 THE_ANSWER_TO_EVERYTHING = 42
 
