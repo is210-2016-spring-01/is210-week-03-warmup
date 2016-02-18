d0a7f835770c64b7530e8e18fe5925ed2e96e7fb
Vanessa Zuniga <vanessa.zuniga@spsmail.cuny.edugit commit -m added>
2016-02-18 17:18:12 +0000

    a mathematical statement

---
+++ task_02.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This is a mathematical statement."""
+
+WEEKS = 19 % 10
+
+print 19 % 10
+
+WEEKS = 9 + 100
+WEEKS = 109 + (2 ** 8)
+WEEKS = 365 // 7
+
+print WEEKS
