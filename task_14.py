5f96b2cb2ff95a10632ffe89a5fc310ee7b619e2
Vanessa Zuniga <vanessa.zuniga@spsmail.cuny.edugit commit -m added>
2016-02-18 17:44:15 +0000

    the 'AND and equality operator

---
+++ task_14.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""The logical AND operator and equality operator."""
+
+IS_TRUE = True
+IS_FALSE = False
+IS_NONE = None
+
+print IS_TRUE
+print IS_FALSE
+print IS_NONE
