ccac49914ae381e87dada2346a44eddd56deb4cf
Vanessa Zuniga <vanessa.zuniga@spsmail.cuny.edugit commit -m added>
2016-02-25 07:17:46 +0000

    added a new variable name

--- task_10.py
+++ task_10.py
@@ -3,6 +3,7 @@
 """Task 10"""
 
 MOVIE = 'dr. strangelove or: how i learned to stop worrying and love the bomb'
-print MOVIE.upper()
+
 TITLECASE = MOVIE.upper()
-print TITLECASE
+ENTITLED = TITLECASE
+print ENTITLED
