1c6b6770a76b342f9d9d176304811254bb2ac3b6
Elisha Chauhan <elisha.chauhan@spsmail.cuny.edu>
2016-02-29 21:48:57 +0000

    Change file through style formatting

---
+++ task_12.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""A simple docstring"""
+
+import decimal
+
+import fractions
+
+INVAL = 1
+
+FLOATVAL = 0.1
+
+DECVAL = decimal.Decimal('0.1')
+
+FRACVAL = fractions.Fraction(1, 10)
