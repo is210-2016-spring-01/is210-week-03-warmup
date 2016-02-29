d78cf3f8154fd4eb320033374b0f942ff18daee9
Elisha Chauhan <elisha.chauhan@spsmail.cuny.edu>
2016-02-29 21:31:04 +0000

    Updated file through style formatting

--- task_09.py
+++ task_09.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """A computer that can build the question.
-Who has the question at hand?"""
+
+Who has the question at hand?
+"""
 
 THE_ANSWER_TO_EVERYTHING = 42
 
