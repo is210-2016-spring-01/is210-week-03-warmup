7b0f1927986fc2a719c1f74cdc19645e53bf5cf4
Elisha Chauhan <elisha.chauhan@spsmail.cuny.edu>
2016-02-29 21:15:58 +0000

    Change file through style formatting

--- task_05.py
+++ task_05.py
@@ -6,9 +6,4 @@
 TEENAGE_MUTANT_NINJAS = ('Michaelangelo. Leonardo. Rafael. Donatello. Heroes '
                          'in a half shell.')
 
-print TEENAGE_MUTANT_NINJAS.split('. ')
-
-TURTLE_POWER = (['Michaelangelo', 'Leonardo', 'Rafael', 'Donatello', 'Heroes '
-                'in a half shell.'])
-
-print TURTLE_POWER
+TURTLE_POWER = TEENAGE_MUTANT_NINJAS.split('. ')
